369d54d179e04f5194c75b046a1a5c32030f4496
AnushkaWijegoonawardana97 <anushkaduwolka123@gmail.com>
2023-11-26 11:16:52 +0530

    MultiLingualStringTable.java Refactoring 12: Adding meaning full variable name

--- src/base/MultiLingualStringTable.java
+++ src/base/MultiLingualStringTable.java
@@ -10,15 +10,16 @@ public class MultiLingualStringTable {
 		English, Klingon
 	}
 
-	private static LanguageSetting cl = LanguageSetting.English;
-	private static String[] em = { "Enter your name:", "Welcome", "Have a good time playing Abominodo" };
-	private static String[] km = { "'el lIj pong:", "nuqneH", "QaQ poH Abominodo" };
+	private static LanguageSetting currentSelectedLanguage = LanguageSetting.English;
+	private static String[] englishLanguageMessage = { "Enter your name:", "Welcome",
+			"Have a good time playing Abominodo" };
+	private static String[] klingonLanguageMessage = { "'el lIj pong:", "nuqneH", "QaQ poH Abominodo" };
 
 	public static String getMessage(int index) {
-		if (cl == LanguageSetting.English) {
-			return em[index];
+		if (currentSelectedLanguage == LanguageSetting.English) {
+			return englishLanguageMessage[index];
 		} else {
-			return km[index];
+			return klingonLanguageMessage[index];
 		}
 
 	}
