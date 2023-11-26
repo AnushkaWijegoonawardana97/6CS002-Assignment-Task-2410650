f6f4df0699c3506923ccb595bc6b9e3800c9f7c7
AnushkaWijegoonawardana97 <anushkaduwolka123@gmail.com>
2023-11-26 09:14:18 +0530

    _Q.java Refactoring 02: Adding Comments

--- src/base/QuoteContent.java
+++ src/base/QuoteContent.java
@@ -1,6 +1,11 @@
 package base;
 
+/**
+ * The {@code QuoteContent} class represents a collection of quotes with their
+ * object.
+ */
 public class QuoteContent {
+	// The array of strings holding quote and author information
 	static transient String[] stuff = { "Progress comes from the intelligent use of experience.", "Elbert Hubbard", "",
 			"No amount of experimentation can ever prove me right; a single experiment can prove me wrong.",
 			"Albert Einstein", "", "To be trusted is a greater complement than to be loved.", "George MacDonald", "",
