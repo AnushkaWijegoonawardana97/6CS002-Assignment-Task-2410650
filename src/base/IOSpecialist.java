5956d4e2f6f6900219a75958e37e1a75035f76ef
AnushkaWijegoonawardana97 <anushkaduwolka123@gmail.com>
2023-11-26 10:58:58 +0530

    IOLibrary.java Refactoring 11: Adding Comments

--- src/base/IOSpecialist.java
+++ src/base/IOSpecialist.java
@@ -1,14 +1,25 @@
 package base;
 
 /**
- * @author Kevan Buckley, maintained by __student
- * @version 2.0, 2014
+ * The IOSpecialist class provides a simplified interface for handling
+ * input/output operations. It serves as a mediator between the client code and
+ * the IOLibrary, encapsulating input-related functionality.
+ * 
+ * Author: Kevan Buckley, maintained by __student Version: 2.0, 2014
  */
 
 public class IOSpecialist {
+	/**
+	 * Default constructor for the IOSpecialist class.
+	 */
 	public IOSpecialist() {
 	}
 
+	/**
+	 * Retrieves a string from the console input using the IOLibrary.
+	 * 
+	 * @return The string entered by the user.
+	 */
 	public String getString() {
 		return IOLibrary.getString();
 	}
