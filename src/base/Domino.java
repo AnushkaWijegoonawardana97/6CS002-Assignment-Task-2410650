1d4fdd0e43ac0ccbb81574c489cdf17fbcee52e9
AnushkaWijegoonawardana97 <anushkaduwolka123@gmail.com>
2023-11-26 10:29:31 +0530

    Domino.java Refactoring 08: Adding Comments

--- src/base/Domino.java
+++ src/base/Domino.java
@@ -1,8 +1,10 @@
 package base;
 
 /**
- * @author Kevan Buckley, maintained by __student
- * @version 2.0, 2014
+ * The Domino class represents a domino with high and low values. It implements
+ * Comparable to support sorting of dominoes.
+ * 
+ * Author: Kevan Buckley, maintained by __student Version: 2.0, 2014
  */
 
 public class Domino implements Comparable<Domino> {
@@ -14,20 +16,26 @@ public class Domino implements Comparable<Domino> {
 	private int ly;
 	private boolean placed = false;
 
+	/**
+	 * Constructs a Domino object with specified high and low values.
+	 * 
+	 * @param high The high value of the domino.
+	 * @param low  The low value of the domino.
+	 */
 	public Domino(int high, int low) {
 		super();
 		this.setHigh(high);
 		this.setLow(low);
 	}
 
-	public int getHigh() {
-		return high;
-	}
-
-	public void setHigh(int high) {
-		this.high = high;
-	}
-
+	/**
+	 * Places the domino on the grid with specified coordinates.
+	 * 
+	 * @param hx The x-coordinate of the higher end.
+	 * @param hy The y-coordinate of the higher end.
+	 * @param lx The x-coordinate of the lower end.
+	 * @param ly The y-coordinate of the lower end.
+	 */
 	public void place(int hx, int hy, int lx, int ly) {
 		this.setHx(hx);
 		this.setHy(hy);
@@ -36,6 +44,11 @@ public class Domino implements Comparable<Domino> {
 		setPlaced(true);
 	}
 
+	/**
+	 * Converts the domino to a string representation.
+	 * 
+	 * @return The string representation of the domino.
+	 */
 	public String toString() {
 		StringBuffer result = new StringBuffer();
 		result.append("[");
@@ -67,8 +80,9 @@ public class Domino implements Comparable<Domino> {
 		result.append(")");
 	}
 
-	/** turn the domino around 180 degrees clockwise */
-
+	/**
+	 * Inverts the domino by turning it around 180 degrees clockwise.
+	 */
 	public void invert() {
 		int tx = getHx();
 		setHx(getLx());
@@ -79,6 +93,11 @@ public class Domino implements Comparable<Domino> {
 		setLy(ty);
 	}
 
+	/**
+	 * Checks if the domino is horizontal.
+	 * 
+	 * @return true if the domino is horizontal, false otherwise.
+	 */
 	public boolean ishl() {
 		return getHy() == getLy();
 	}
@@ -88,6 +107,15 @@ public class Domino implements Comparable<Domino> {
 			return 1;
 		}
 		return this.getLow() - arg0.getLow();
+	}
+
+	// Getters and setters for high, low, hx, hy, lx, ly, and placed
+	public int getHigh() {
+		return high;
+	}
+
+	public void setHigh(int high) {
+		this.high = high;
 	}
 
 	public int getLow() {
