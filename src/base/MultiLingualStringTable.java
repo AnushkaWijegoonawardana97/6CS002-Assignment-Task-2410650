a8936ff5b099f7c15c72ae4e3f04f03345a0f582
AnushkaWijegoonawardana97 <anushkaduwolka123@gmail.com>
2023-11-26 11:21:54 +0530

    MultiLingualStringTable.java Refactoring 13: Adding comments

--- src/base/MultiLingualStringTable.java
+++ src/base/MultiLingualStringTable.java
@@ -1,20 +1,43 @@
 package base;
 
 /**
- * @author Kevan Buckley, maintained by __student
- * @version 2.0, 2014
+ * The MultiLingualStringTable class provides a mechanism for managing and
+ * retrieving messages in different languages.
+ * 
+ * Author: Kevan Buckley, maintained by __student Version: 2.0, 2014
  */
 
 public class MultiLingualStringTable {
+	/**
+	 * Enumeration representing the available language settings.
+	 */
 	private enum LanguageSetting {
 		English, Klingon
 	}
 
+	/**
+	 * The currently selected language setting.
+	 */
 	private static LanguageSetting currentSelectedLanguage = LanguageSetting.English;
+
+	/**
+	 * Array of messages in English language.
+	 */
 	private static String[] englishLanguageMessage = { "Enter your name:", "Welcome",
 			"Have a good time playing Abominodo" };
+
+	/**
+	 * Array of messages in Klingon language.
+	 */
 	private static String[] klingonLanguageMessage = { "'el lIj pong:", "nuqneH", "QaQ poH Abominodo" };
 
+	/**
+	 * Retrieves the message at the specified index based on the current language
+	 * setting.
+	 *
+	 * @param index The index of the message to retrieve.
+	 * @return The message in the current language at the specified index.
+	 */
 	public static String getMessage(int index) {
 		if (currentSelectedLanguage == LanguageSetting.English) {
 			return englishLanguageMessage[index];
